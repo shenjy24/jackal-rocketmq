124324978839ca8748ba31d74e46319ad5bd3cc7
shenjy <shenjiayun@hzyushi.cn>
2018-11-26 13:44:22 +0800

    添加延迟消息

---
+++ src/main/java/com/jonsa/producer/ScheduledProducer.java
@@ -0,0 +1,38 @@
+package com.jonsa.producer;
+
+import com.jonsa.common.MqProp;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+
+/**
+ * 【生产者：发送延迟消息】
+ *
+ * @author shenjy 2017/11/17
+ */
+public class ScheduledProducer {
+
+    public void produce(String topic, String tag, String key) throws MQClientException, InterruptedException{
+        DefaultMQProducer producer = new DefaultMQProducer(MqProp.PRODUCER_GROUP);
+        producer.setNamesrvAddr(MqProp.NAME_SERVER_ADDR);
+        producer.start();
+
+        for (int i = 0; i < 100; i++) {
+            try {
+                if (i % 2 == 0) {
+                    Message msg = new Message(topic, tag, key, ("Hello RocketMq " + i).getBytes("UTF-8"));
+                    msg.setDelayTimeLevel(3);
+                    SendResult sendResult = producer.send(msg);
+                    System.out.println(sendResult);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                Thread.sleep(1000);
+            }
+
+        }
+
+        producer.shutdown();
+    }
+}
